ac273a0c65647a86760e38791a4735db2258c8ca
GFG13 <gfgambino@gmail.com>
2020-02-26 00:07:40 -0500

    initial commit

--- beam-bending/constants.py
+++ beam-bending/constants.py
@@ -2,11 +2,11 @@ import scipy.constants as sc
 
 grav = sc.g;                # [m/s^2] gravity
 total_span = 150;           # [mm] beam span
-P_span = 1/3*total_span;    # [-] loading span
+P_span = 1/3*total_span;    # [mm] loading span
 
 d_init = .038               # [m]
 x_rock = 0.15               # [m]
 x_ice = 0.01                # [m]
 rho_rock = 2600             # [kg/m3]
 rho_ice = 1000              # [kg/m3]
-P = 0*grav                 # [N]
+P = 50*grav                 # [N]
